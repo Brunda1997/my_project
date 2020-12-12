f0d204dffde0ea81669ddedca7bf249a628fdcf7
Brunda1997 <43960783+Brunda1997@users.noreply.github.com>
2020-12-12 12:43:54 +0530

    Update Chart.js

--- src/component/Chart.js
+++ src/component/Chart.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Bar, Line} from 'react-chartjs-2';
+import { Line} from 'react-chartjs-2';
 import Notepad from './Notepad';
 import './Chart.css';
 import Tab from './Tabs';
